e0e712a2be95f7529ab9e02bb3f91672e13135b7
Emily Zhang <emily49@stanford.edu>
2023-03-06 22:09:32 -0800

    Revert "Miner functionality"
    This reverts commit d7769d19ace6b7daed5a73bc6772c0e59d816cc8.
    
    tttttttttttttt/semver/CHANGELOG.md
    tttttttttttttt/yn/

--- src/mining.ts
+++ src/mining.ts
@@ -1,59 +1,31 @@
 import { mempool } from "./mempool";
 import { Block, TARGET } from "./block";
 import { chainManager } from "./chain";
-import { AnnotatedError } from "./message";
-import { logger } from "./logger";
-import { objectManager } from "./object";
-import { peerManager } from "./peermanager";
-import { network } from "./network";
-const studentids = ["pmiao", "emily49"];
 
-export class Miner {
-  async mine() {
-    while (true) {
-      const block = this.getNewBlock();
-      if (block) {
-        const minedBlock = this.mineBlock(block);
-        await minedBlock.validate(network.peers[0]);
-        await objectManager.put(minedBlock.toNetworkObject());
-        network.broadcast({
-          type: "ihaveobject",
-          objectid: minedBlock.blockid,
-        });
-        logger.info(
-          `Succesfully mined, validated, and broadcasted block with blockid ${minedBlock.blockid}.`
+const studentids = ["pmiao"];
+
+export class MiningManager {
+    async getNewBlock() {
+        let txids = mempool.getTxIds();
+        let previd = chainManager.longestChainTip?.blockid;
+        return new Block(
+            previd? previd : null,
+            txids,
+            "",
+            TARGET,
+            Math.floor(new Date().getTime() / 1000),
+            undefined,
+            undefined,
+            studentids
         );
-        logger.info(`Full block is: ${minedBlock.toNetworkObject()}`);
-      }
     }
-  }
-  getNewBlock() {
-    let txids = mempool.getTxIds();
-    if (chainManager.longestChainTip == null) {
-      logger.info(`Miner -- No chain exists to mine on.`);
-      return null;
-    } else {
-      let previd = chainManager.longestChainTip?.blockid;
-      return new Block(
-        previd ? previd : null,
-        txids,
-        "",
-        TARGET,
-        Math.floor(new Date().getTime() / 1000),
-        undefined,
-        undefined,
-        studentids
-      );
-    }
-  }
 
-  mineBlock(block: Block) {
-    let nonce = 0;
-    do {
-      logger.debug(`Miner -- Trying nonce of ${nonce}`);
-      block.nonce = String(nonce);
-      nonce++;
-    } while (!block.hasPoW());
-    return block;
-  }
+    async mineBlock(block: Block) {
+        let nonce = 0;
+        do {
+            block.nonce = String(nonce);
+            nonce++;
+        } while (!block.hasPoW());
+        return block;
+    }
 }
